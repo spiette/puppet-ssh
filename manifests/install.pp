566b35b97c3616d4287bc90c42d247a9a0c68166
Simon Piette <piette.simon@gmail.com>
2013-03-29 18:10:31 -0400

    add autoupdate param to ssh::install

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,16 @@
 # == Class ssh::intall
 # This class is meant to be called from ssh
 # It install requires packages
-class ssh::install {
+class ssh::install (
+  $autoupdate=false,
+  ) {
   include ssh::params
+  $ensure = $autoupdate ? {
+    false   => present,
+    true    => latest,
+    default => fail('autoupdate should be true or false')
+  }
   package { $ssh::params::pkgname:
-    ensure => present,
+    ensure => $ensure,
   }
 }
---
+++ spec/classes/ssh_install_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'ssh::install' do
+  let(:title) { 'ssh::install' }
+
+  let(:facts) { {
+    :osfamily               => 'Debian',
+    :operatingsystem        => 'Debian',
+    :operatingsystemrelease => '7',
+  } }
+
+  context "no autoupdate" do
+    it { should create_package('openssh-server').with_ensure('present') }
+  end
+  context "autoupdate" do
+    let(:params) { { :autoupdate => true } } 
+    it { should create_package('openssh-server').with_ensure('latest') }
+  end
+  context "autoupdate wrong argument" do
+    let(:params) { { :autoupdate => 'true' } } 
+    it { expect {
+      create_package('openssh-server').to\
+        raise_error(Puppet::Error, /autoupdate should be true or false/)
+    } }
+  end
+end
