25496937df58a70b0e005ef2c396c7026468dabe
Ben Priestman <ben.priestman@immediate.co.uk>
2014-08-08 13:52:24 +0100

    extending ssh::match to be able to handle a hash of conditions. This allows us to specify multiple conditions which a single match block will match.

--- manifests/match.pp
+++ manifests/match.pp
@@ -6,30 +6,42 @@
 # == Parameters
 #
 # [*type*]
-#   Object type affected by the following options.
+#   Depreciated. For backwards-compatibility, this parameter defines the object type in a single match condition.
 #
 # [*pattern*]
-#   Pattern to be matched against. Defaults to $name
+#   Depreciated. When matching on a single condition, the pattern to be matched against. Defaults to $name
 #
+# [*conditions*]
+#   A hash of type/pattern pairs to use in matching on multiple conditions. Has no effect, if $type is defined.
+# 
 # [*options*]
 #   A hash of options to set for the user. Defaults to {}. Will fail if empty.
 #
-define ssh::match ($type, $options, $pattern=$name) {
-  include ssh
-  if $type in [
+define ssh::match (
+  $type       = undef,
+  $pattern    = $name,
+  $conditions = undef,
+  $options,
+) {
+  if $type != undef and !($type in [
     'user',
     'group',
     'host',
     'localaddress',
     'localport',
     'address',
-  ] {
-    concat::fragment { "ssh_match_${type}_${pattern}":
+  ]) {
+    fail("Unknown type match: ${type}")
+  } elsif $type == undef and $conditions == undef {
+    fail("Either \$type or \$conditions must be specified.") 
+  } elsif $conditions != undef and !(is_hash($conditions)) {
+    fail("\$conditions must be an array")
+  } else {
+    include ssh
+    concat::fragment { "ssh_match_${name}":
       target  => $ssh::params::conffile,
       order   => 20,
       content => template('ssh/sshd_config_match.erb')
     }
-  } else {
-    fail("Unknown type match: ${type}")
   }
 }
